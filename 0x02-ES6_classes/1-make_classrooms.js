ca2b1c740783c5be2df544ab13cd02dc1e967a2a
Titilope Mogaji <104643681+TitiMogaji@users.noreply.github.com>
2023-07-28 06:01:43 +0100

    commit and push

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const arr = [];
+  arr[0] = new ClassRoom(19);
+  arr[1] = new ClassRoom(20);
+  arr[2] = new ClassRoom(34);
+
+  return arr;
+}
