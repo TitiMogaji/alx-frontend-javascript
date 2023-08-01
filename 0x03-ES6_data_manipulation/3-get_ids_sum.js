28317938e702492fb1e4930be13ba8976b80888b
Titilope Mogaji <104643681+TitiMogaji@users.noreply.github.com>
2023-08-01 15:47:47 +0100

    commit & update 0x03-ES6_data_manipulation

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -5,7 +5,7 @@
  *   firstName: String,
  *   location: String
  * }[]} students - The list of students.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Titi Mogaji <https://github.com/TitiMogaji>
  * @returns {Number}
  */
 export default function getStudentIdsSum(students) {
--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,7 +1,7 @@
 /**
  * Creates a set from an array.
  * @param {Array} array - The source array.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Titi Mogaji <https://github.com/TitiMogaji>
  * @returns {Set}
  */
 export default function setFromArray(array) {
